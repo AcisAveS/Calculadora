903e136d3d70b1ea05d1f9a1df64ff04009449bf
Acis A.S <acisavendano@gmail.com>
2023-04-16 17:00:59 -0700

    Se agregaron los operadores %, 'x' elevado a 'y', se acomodaron los botones y se eliminaron las impresiones de consola

--- Ventana.java
+++ Ventana.java
@@ -1,6 +1,5 @@
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -19,8 +18,10 @@ import javax.swing.JLabel;
 public class Ventana extends javax.swing.JFrame implements ActionListener {
 
     private JLabel valor = new JLabel("0");
+    private double primerValor = 0;
 
     public static void main(String args[]) {
+        // Definir el tipo de fuente, estilo y tamaño a todos los botones y campos
         UIManager.put("Button.font", new Font("Arial", Font.PLAIN, 20));
         UIManager.put("Label.font", new Font("Arial", Font.BOLD, 30));
         new Ventana();
@@ -33,7 +34,7 @@ public class Ventana extends javax.swing.JFrame implements ActionListener {
 
         add(Contenedor());
 
-        setSize(new Dimension(400, 500));
+        setSize(new Dimension(400, 600));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
@@ -43,22 +44,20 @@ public class Ventana extends javax.swing.JFrame implements ActionListener {
     private JPanel Botones() {
         JPanel botones = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        JButton operaciones[] = new JButton[8];
+        JButton operaciones[] = new JButton[9];
         JButton numeros[] = new JButton[10];
         JButton punto = Boton(new JButton(), ".");
-        JButton resultado = Boton(new JButton(), ".");
+        JButton resultado = Boton(new JButton(), "=");
         int cont = 0;
 
         /*
          * Operadores
-         * lenguje Unicode para mostrar el valor de x al cuadrador u00B2
-         * lenguje Unicode para mostrar raiz cuadrada de x u221A
-         * { "%", "+", "-","/", "x\u00B2", "\u221Ax" }
+         * lenguaje Unicode para mostrar el valor de x elevado a la x 02B8
+         * lenguaje Unicode para mostrar el valor de x al cuadrado u00B2
+         * lenguaje Unicode para mostrar raiz cuadrada de x u221A
          */
-        String op[] = { "CE", "C", "x\u00B2", "\u221Ax", "%", "+", "-", "/" };
-
+        String op[] = { "C", "CE", "%", "x\u02B8", "x\u00B2", "\u221Ax", "/", "X", "+", "-" };
         ArrayList<String> operadores = new ArrayList<String>();
-
         for (String e : op)
             operadores.add(e);
 
@@ -66,20 +65,34 @@ public class Ventana extends javax.swing.JFrame implements ActionListener {
         gbc.gridy = 0;
         gbc.ipadx = 30;
         gbc.ipady = 30;
+        gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.BOTH;
         gbc.insets = new Insets(4, 4, 4, 4);
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= 6; i++) {
+
+            switch (i) {
+                case 1:
+                    gbc.gridx = 2;
+                    gbc.gridwidth = 1;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    gbc.gridx = 0;
+                    gbc.gridy++;
+                    break;
+            }
+
             operaciones[i] = Boton(operaciones[i], operadores.get(0));
             botones.add(operaciones[i], gbc);
-
             operadores.remove(0);
-            gbc.gridx++;
             cont = i;
+            gbc.gridx++;
         }
 
         gbc.gridx = 0;
-        gbc.gridy = 1;
+        gbc.gridy = 2;
 
         for (int i = 9; i >= 0; i--) {
             numeros[i] = Boton(numeros[i], i + "");
@@ -88,7 +101,6 @@ public class Ventana extends javax.swing.JFrame implements ActionListener {
             gbc.gridx++;
 
             if (gbc.gridx == 3) {
-
                 operaciones[cont] = Boton(operaciones[cont], operadores.get(0));
                 botones.add(operaciones[cont], gbc);
 
@@ -98,7 +110,7 @@ public class Ventana extends javax.swing.JFrame implements ActionListener {
                 cont++;
 
                 // Posicionar el numero 0 en el centro de los numeros
-                if (gbc.gridy == 4)
+                if (gbc.gridy == 5)
                     gbc.gridx++;
             }
         }
@@ -126,7 +138,6 @@ public class Ventana extends javax.swing.JFrame implements ActionListener {
         String val = ((JButton) e.getSource()).getName();
 
         if (Character.isDigit(val.toCharArray()[0])) {
-            System.out.println(valor.getText());
 
             if (valor.getText() != "0") {
                 String concatenar = valor.getText();
@@ -136,6 +147,21 @@ public class Ventana extends javax.swing.JFrame implements ActionListener {
             }
 
         } else {
+            if (val == "CE") {
+
+                valor.setText("0");
+
+            } else if (val == "C") {
+
+                valor.setText("0");
+                primerValor = 0;
+
+            } else {
+                if (primerValor != 0) {
+
+                }
+                primerValor = Double.parseDouble(valor.getText());
+            }
         }
     }
 
